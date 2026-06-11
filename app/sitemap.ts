0f98ed8e5ed8be1293ced9ba43bb0a3da83d0299
AI PC <atkola12345@gmail.com>
2026-06-11 23:43:59 +0530

    fix: sitemaps, robots.txt to work with Google Search

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://imagely.app';
+  const baseUrl = 'https://www.imagely.app';
 
   return [
     {
